a6303a0c461fa4516f74b3c7eddbddb11b6a6869
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2014-07-16 15:07:41 +0200

    work in progress

--- api/lib/get_wikipage_egonetwork.py
+++ api/lib/get_wikipage_egonetwork.py
@@ -1,85 +1,113 @@
 #!/usr/bin/env python
 
-import uuid, requests, re, json, os
-import networkx as nx
+import uuid, re, json, os
+import requests
+import networkx
 from networkx.readwrite import json_graph
 
 # TODO
+# cache results of one page with timestamp
+# return filtered network
 # add read controversiality in sqlite)
 
-class WikipageNetwork(Object):
+chunkize = lambda a, n: [a[i:i+n] for i in xrange(0, len(a), n)]
+
+class WikipageNetwork(object):
 
     regex_links = re.compile(r'href="/wiki/([^"]+)"')
+    regex_anchors = re.compile(r'#.+$')
     link_filters = ['File', 'User', 'Category', 'Help', 'Portal', 'Talk',
-        'Wikipedia', 'Template', 'Template_talk', 'Special',
-        'Category', 'Book']
+        'Wikipedia', 'Template', 'Special', 'Draft', 'wikipedia',
+        'Category', 'Book', 'User']
 
-    def __init__(self, title="Global_warming", language="en", token=None, cache_redirs={}):
+    def __init__(self, token=None, title="Global_warming", language="en", cache_redirs={}):
+        if not os.path.isdir("cache"):
+            os.makedirs("cache")
+        self.curid = 0
         self.cache_redirs = cache_redirs
+        self.index_pages = {}
+        self.done_pages = []
         if token:
             self.token = token
             self.reload_network()
         else:
             self.token = uuid.uuid1()
-            self.init_network(title, language)
-            self.add_page(title)
+            self.init_network(self.clean_page(title), language)
 
-    def init_network(title, language):
-        self.title = title
-        self.language = language
-        self.root_api_url = "https://%s.wikipedia.org/w/api.php?action=query&format=json" % language
-        self.done_pages = [title]
-        self.network = nx.DiGraph()
-        self.add_node(title)
-        self.networkfile = os.path.join("cache", "%s.json" % self.token)
-
-    def get_jsonfile():
-        return os.path.join("cache", "%s-metas.json" % self.token)
-
-    def reload_network():
-        with open(self.get_jsonfile()) as f:
-            data = json.load(f)
-        self.init_network(data["title"], data["language"])
-        self.done_pages = data["nodes"].keys()
-        with open(self.networkfile) as f:
-            self.network = json.load(f)
-
-    def save():
-        with open(self.get_jsonfile()) as f:
-            json.dump({
-                "title": self.title,
-                "language": self.language,
-                "pages": self.done_pages
-            })
-        with open(self.networkfile) as f:
-            json.dump(json_graph.node_link_data(self.network), f)
-
-    def add_node(page):
-        if page not in self.network:
-            self.network.add_node(title, controversiality=0)
-
-    def add_link(frompage, topage):
-        pass
-
-    def explore_node(title):
-        get_outlinks(title)
-        get_inlinks(title)
-
-    def add_page(title):
-        explore_node(title)
+    def add_page(self, page):
+        page = self.clean_page(page)
+        self.add_node(page)
+        self.get_outlinks(page)
+        self.get_inlinks(page)
+        self.done_pages.append(page)
         self.save()
         return self.return_filtered_network()
 
-    def return_filtered_network():
+    def clean_page(self, page):
+        page = self.regex_anchors.sub('', page)
+        return page.replace("_", " ")
+
+    def init_network(self, title, language):
+        self.title = title
+        self.language = language
+        self.root_api_url = "https://%s.wikipedia.org/w/api.php?action=query&format=json" % language
+        self.network = networkx.DiGraph()
+        self.add_node(title)
+        self.networkfile = os.path.join("cache", "%s.json" % self.token)
+
+    def get_jsonfile(self):
+        return os.path.join("cache", "%s-metas.json" % self.token)
+
+    def reload_network(self, ):
+        with open(self.get_jsonfile()) as f:
+            data = json.load(f)
+        self.init_network(data["title"], data["language"])
+        self.done_pages = data["pages"]
+        self.index_pages = data["index"]
+        with open(self.networkfile) as f:
+            self.network = json.load(f)
+
+    def save(self):
+        with open(self.get_jsonfile(), "w") as f:
+            json.dump({
+                "title": self.title,
+                "language": self.language,
+                "pages": self.done_pages,
+                "index": self.index_pages
+            }, f)
+        with open(self.networkfile, "w") as f:
+            json.dump(json_graph.node_link_data(self.network), f)
+
+    def add_node(self, page):
+        if page not in self.network.nodes():
+            contro = 0
+            #contro = sqlite.get value
+            self.network.add_node(self.curid, label=page, controversiality=contro)
+            self.index_pages[page] = self.curid
+            self.curid += 1
+            return self.curid
+        return self.index_pages[page]
+
+    def add_edge(self, frompage, topage):
+        idf = self.add_node(frompage)
+        idt = self.add_node(topage)
+        if not self.network.has_edge(idf, idt):
+            self.network.add_edge(idf, idt)
+
+    def return_filtered_network(self):
         pass
 
-    def filter_link(link):
+    def filter_link(self, link):
+        if link == u"Main_Page":
+            return True
         for filter in self.link_filters:
             if link.startswith("%s:" % filter):
                 return True
+            if link.startswith("%s talk:" % filter):
+                return True
         return False
 
-    def solve_redirects(pages):
+    def solve_redirects(self, pages):
         tosolve = []
         for p in list(pages):
             if p not in self.cache_redirs:
@@ -87,45 +115,58 @@ class WikipageNetwork(Object):
             else:
                 pages.remove(p)
                 pages.append(self.cache_redirs[p])
-        redir_api_url = "%s&redirects&titles=%s" % (root_api_url, "|".join(tosolve))
-        data = json.loads(requests.post(redir_api_url).text)
-        if "normalized" in data["query"]:
-            for redir in data["query"]["normalized"]:
-                pages.remove(redir["from"])
-                pages.append(redir["to"])
-                self.cache_redirs[redir["from"]] = redir["to"]
+        for chunk in chunkize(tosolve, 300):
+            redir_api_args = {
+                "redirects": 1,
+                "titles": "|".join(chunk)
+            }
+            temp = requests.post(self.root_api_url, params=redir_api_args)
+            data = json.loads(temp.text)
+            if "normalized" in data["query"]:
+                print pages
+                for redir in data["query"]["normalized"]:
+                    print redir
+                    pages.remove(redir["from"])
+                    pages.append(redir["to"])
+                    self.cache_redirs[redir["from"]] = redir["to"]
 
-    def get_outlinks(page):
+    def get_outlinks(self, page):
         out_links = []
         if page in self.done_pages:
             return
         url = "https://%s.wikipedia.org/wiki/%s" % (self.language, page)
-        page = requests.get(url).text
-        for link in self.regex_links.findall(page):
-            if filter_link(link) or link in out_links:
+        htmlcontent = requests.get(url).text
+        for link in self.regex_links.findall(htmlcontent):
+            link = self.clean_page(link)
+            if self.filter_link(link) or link in out_links:
                 continue
             out_links.append(link)
-        solve_redirects(out_links)
-        return out_links
+        self.solve_redirects(out_links)
+        for link in out_links:
+            self.add_edge(page, link)
 
-    def get_inlinks(page):
+    def get_inlinks(self, page):
         if page in self.done_pages:
             return
-        api_url = "%s&list=backlinks&blredirect&blfilterredir=nonredirects&bllimit=500&bltitle=%s" % (root_api_url, title)
+        api_url = "%s&list=backlinks&blredirect&blfilterredir=nonredirects&bllimit=500&bltitle=%s" % (self.root_api_url, page)
         cur_api_url = api_url
         while cur_api_url:
             data = json.loads(requests.get(cur_api_url).text)
             for link in data["query"]["backlinks"]:
                 lk = link["title"]
-                if filter_link(lk) or lk in in_links:
+                lk = self.clean_page(lk)
+                if self.filter_link(lk):
                     continue
-                in_links.append(lk)
+                self.add_edge(lk, page)
             if "query-continue" in data:
                 cur_api_url = "%s&blcontinue=%s" % (api_url, data["query-continue"]["backlinks"]["blcontinue"])
             else:
                 cur_api_url = ""
-        return in_links
 
-if __name__ ==  __main__:
-    net = WikipageNetwork()
-    print len(net.in_links), len(net.out_links)
+if __name__ ==  '__main__':
+    net = WikipageNetwork(None, "Global_warming", "en")
+    filtered_netw = net.add_page("Global_warming")
+    cache_redirs = net.cache_redirs
+    token = net.token
+    net2 = WikipageNetwork(token, cache_redirs=cache_redirs)
+    new_filtered_netw = net2.add_page("Climate_change")
